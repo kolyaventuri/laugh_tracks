6b222e28d9393e555193f3c6b410a295e8bada52
Kolya Venturi <kolya.venturi@enx3s.com>
2018-03-16 10:57:42 -0600

    Find comedians with specific age

--- app/controllers/laugh_tracks_app.rb
+++ app/controllers/laugh_tracks_app.rb
@@ -1,7 +1,12 @@
 # Defines our comedian list app
 class LaughTracksApp < Sinatra::Base
   get '/comedians' do
-    @comedians = Comedian.all
+    @comedians = if params[:age].nil?
+                   Comedian.all
+                 else
+                   Comedian.where(age: params[:age])
+                 end
+
     erb :comedians
   end
 end
